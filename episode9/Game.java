c3235c55c444ae7bf436de4478073ba3e5c93e9c
Scott Pullen <s.pullen05@gmail.com>
2012-10-13 13:45:51 -0400

    adding episode 9

---
+++ episode9/Game.java
@@ -0,0 +1,97 @@
+package rain;
+
+import java.awt.Canvas; 
+import java.awt.Color;
+import java.awt.Dimension; 
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+
+import javax.swing.JFrame;
+
+import rain.graphics.Screen;
+
+public class Game extends Canvas implements Runnable { 
+	private static final long serialVersionUID = 1L;
+
+	public static int width = 300;
+	public static int height = width / 16 * 9;
+	public static int scale = 3;
+	
+	private Thread thread;
+	private JFrame frame;
+	private boolean running = false;
+	
+	private Screen screen;
+	
+	private BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+	private int[] pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
+	
+	public Game() {
+	    Dimension size = new Dimension(width * scale, height * scale);
+	    setPreferredSize(size);
+	
+	    screen = new Screen(width, height);
+	    
+	    frame = new JFrame();
+	}
+	
+	public synchronized void start() {
+	    running = true;
+	    thread = new Thread(this, "Display");
+	    thread.start();
+	}
+	
+	public synchronized void stop() {
+	    running = false;
+	    try {
+	        thread.join();
+	    } catch (InterruptedException e) {
+	        e.printStackTrace();
+	    }
+	}
+	
+	public void run() {
+	    while (running) {
+	    	update();
+	        render();
+	    }
+	}
+	
+	public void update() {
+	}
+	
+	public void render() {
+	    BufferStrategy bs = getBufferStrategy();
+	    if (bs == null) {
+	        createBufferStrategy(3);
+	        return;
+	    }
+	    
+	    screen.render();
+	    
+	    for(int i = 0; i < pixels.length; i++) {
+	    	pixels[i] = screen.pixels[i];
+	    }
+	    
+	    Graphics g = bs.getDrawGraphics();
+	    g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
+	    g.dispose();
+	    bs.show();
+	}
+	
+	public static void main(String[] args) {
+	    Game game = new Game();
+	    game.frame.setResizable(false);
+		game.frame.setTitle("Rain");
+		game.frame.add(game);
+		game.frame.pack();
+		game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		game.frame.setLocationRelativeTo(null);
+		game.frame.setVisible(true);
+	    
+	    game.start();
+	}
+
+}

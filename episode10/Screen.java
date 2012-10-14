3fe25f462e1cc29236dc9e2a7ff48cfc7525ae1e
Scott Pullen <s.pullen05@gmail.com>
2012-10-14 09:53:03 -0400

    adding episode 10

---
+++ episode10/Screen.java
@@ -0,0 +1,29 @@
+package rain.graphics;
+
+public class Screen {
+
+	private int width, height;	
+	public int[] pixels;
+	
+	public Screen(int width, int height) {
+		this.width = width;
+		this.height = height;
+		
+		pixels = new int[width * height];
+	}
+
+        public void clear() {
+                for(int i = 0; i < pixels.length; i++) {
+                        pixels[i] = 0;
+                }
+        }
+	
+	public void render() {
+		for(int y = 0; y < height; y++) {
+			for(int x = 0; x < width; x++) {
+				pixels[x + y * width] = 0xFF00FF;
+			}
+		}
+	}
+	
+}

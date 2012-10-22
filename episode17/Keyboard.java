1751d01acb41daf23df28755dc7a3760dd1d875f
Scott Pullen <s.pullen05@gmail.com>
2012-10-22 08:22:05 -0400

    Adding episode 17

---
+++ episode17/Keyboard.java
@@ -0,0 +1,33 @@
+package rain.input;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Keyboard implements KeyListener {
+
+	private boolean[] keys = new boolean[120];
+	
+	public boolean up, down, left, right;
+	
+	public void update() {
+		up = keys[KeyEvent.VK_UP] || keys[KeyEvent.VK_W];
+		down = keys[KeyEvent.VK_DOWN] || keys[KeyEvent.VK_S];
+		left = keys[KeyEvent.VK_LEFT] || keys[KeyEvent.VK_A];
+		right = keys[KeyEvent.VK_RIGHT] || keys[KeyEvent.VK_D];
+		
+		System.out.println("up " + up + ", down " + down + ", left " + left + ", right " + right);
+	}
+	
+	public void keyPressed(KeyEvent e) {
+		keys[e.getKeyCode()] = true;
+	}
+
+	public void keyReleased(KeyEvent e) {
+		keys[e.getKeyCode()] = false;
+	}
+	
+	public void keyTyped(KeyEvent e) {
+		
+	}
+
+}

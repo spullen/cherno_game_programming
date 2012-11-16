47a52b1e27e03e5efb109594579858cfd59afcaf
Scott Pullen <s.pullen05@gmail.com>
2012-11-15 20:01:27 -0500

    add episode 19

---
+++ episode19/SpriteSheet.java
@@ -0,0 +1,34 @@
+package rain.graphics;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class SpriteSheet {
+	
+	private String path;
+	private final int SIZE;
+	
+	public int[] pixels;
+	
+	public SpriteSheet(String path, int size) {
+		this.path = path;
+		SIZE = size;
+		pixels = new int[SIZE * SIZE];
+		load();
+	}
+	
+	private void load() {
+		try {
+			BufferedImage image = ImageIO.read(SpriteSheet.class.getResource(path));
+			int w = image.getWidth();
+			int h = image.getHeight();
+			image.getRGB(0, 0, w, h, pixels, 0, w);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
+

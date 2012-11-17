1f665a1b8ec29b7541032d3d85a3bc5db3fdc95a
Scott Pullen <s.pullen05@gmail.com>
2012-11-17 14:47:42 -0500

    adding episode 20

---
+++ episode20/Sprite.java
@@ -0,0 +1,26 @@
+package rain.graphics;
+
+public class Sprite {
+
+	private final int SIZE;
+	private int x, y;
+	private SpriteSheet sheet;
+	public int[] pixels;
+	
+	public Sprite(int size, int x, int y, SpriteSheet sheet) {
+		SIZE = size;
+		pixels = new int[SIZE * SIZE];
+		this.x = x * SIZE;
+		this.y = y * SIZE;
+		this.sheet = sheet;
+		load();
+	}
+	
+	private void load() {
+		for(int y = 0; y < SIZE; y++) {
+			for(int x = 0; x < SIZE; x++) {
+				pixels[x + y * SIZE] = sheet.pixels[(x + this.x) + (y + this.y) * sheet.SIZE];
+			}
+		}
+	}
+}

ab22dcb872af59d37d72ef2f73573902a145d9af
Scott Pullen <s.pullen05@gmail.com>
2012-11-21 16:29:07 -0500

    Adding episode 22

---
+++ episode22/Screen.java
@@ -0,0 +1,43 @@
+package rain.graphics;
+
+import java.util.Random;
+
+public class Screen {
+
+	private int width, height;	
+	public int[] pixels;
+	
+	public final int MAP_SIZE = 64;
+	public final int MAP_SIZE_MASK = MAP_SIZE - 1;
+	
+	public int[] tiles = new int[MAP_SIZE * MAP_SIZE];
+	
+	private Random random = new Random();
+	
+	public Screen(int width, int height) {
+		this.width = width;
+		this.height = height;
+		
+		pixels = new int[width * height];
+		
+		for(int i = 0; i < MAP_SIZE * MAP_SIZE; i++) {
+			tiles[i] = random.nextInt(0xFFFFFF);
+		}
+	}
+	
+	public void clear() {
+		for(int i = 0; i < pixels.length; i++) {
+			pixels[i] = 0;
+		}
+	}
+	
+	public void render(int xOffset, int yOffset) {
+		for(int y = 0; y < height; y++) {
+			int yy = y + yOffset;			
+			for(int x = 0; x < width; x++) {
+				int xx = x + xOffset;
+				pixels[x + y * width] = Sprite.grass.pixels[(x & 15) + (y & 15) * Sprite.grass.SIZE];
+			}
+		}
+	}
+}

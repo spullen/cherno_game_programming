ae513facf478babfb20308c16523507192376d37
Scott Pullen <s.pullen05@gmail.com>
2012-11-28 21:47:09 -0500

    Adding Episode 29

---
+++ episode29/GrassTile.java
@@ -0,0 +1,16 @@
+package rain.level.tile;
+
+import rain.graphics.Screen;
+import rain.graphics.Sprite;
+
+public class GrassTile extends Tile {
+	
+	public GrassTile(Sprite sprite) {
+		super(sprite);
+	}
+	
+	public void render(int x, int y, Screen screen) {
+		// Do render stuff here!
+	}
+
+}

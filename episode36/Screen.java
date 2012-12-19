881a8c99bc6c5d6c03e9ded49662d73790baff34
Scott Pullen <s.pullen05@gmail.com>
2012-12-19 18:59:52 -0500

    fixing bug with the screen renderTile method

--- episode36/Screen.java
+++ episode36/Screen.java
@@ -39,7 +39,7 @@ public class Screen {
 			int ya = y + yp;
 			for(int x = 0; x < tile.sprite.SIZE; x++) {
 				int xa = x + xp;
-				if(xa < 0  || xa >= width || ya < 0 || ya >= width) break;
+				if(xa < 0  || xa >= width || ya < 0 || ya >= height) break;
 				pixels[xa + ya * width] = tile.sprite.pixels[x + y * tile.sprite.SIZE];
 			}
 		}

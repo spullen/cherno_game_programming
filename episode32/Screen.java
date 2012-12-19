36fba7a47fd3d10fe5ab24465e27950dba698779
Scott Pullen <s.pullen05@gmail.com>
2012-12-18 21:49:49 -0500

    fixing bug in render tile method

--- episode32/Screen.java
+++ episode32/Screen.java
@@ -47,9 +47,9 @@ public class Screen {
 
   public void renderTile(int xp, int yp, Tile tile) {
 		for(int y = 0; y < tile.sprite.SIZE; y++) {
-			int ya = y = yp;
+			int ya = y + yp;
 			for(int x = 0; x < tile.sprite.SIZE; x++) {
-				int xa = x = xp;
+				int xa = x + xp;
 				if(xa < 0  || xa >= width || ya < 0 || ya >= width) break;
 				pixels[xa + ya * width] = tile.sprite.pixels[x + y * tile.sprite.SIZE];
 			}

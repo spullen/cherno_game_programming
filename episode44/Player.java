391ee6f3c76c8f38b32c8fcb9a166ce99e38b029
Scott Pullen <s.pullen05@gmail.com>
2013-01-09 18:59:34 -0500

    adding episode45

---
+++ episode44/Player.java
@@ -0,0 +1,33 @@
+package rain.entity.mob;
+
+import rain.input.Keyboard;
+
+public class Player extends Mob {
+	
+	private Keyboard input;
+
+	public Player(Keyboard input) {
+		this.input = input;
+	}
+	
+	public Player(int x, int y, Keyboard input) {
+		this.x = x;
+		this.y = y;
+		this.input = input;
+	}
+	
+	public void update() {
+		int xa = 0, ya = 0;
+		
+		if(input.up) ya--;
+		if(input.down) ya++;
+		if(input.left) xa--;
+		if(input.right) xa++;
+		
+		if(xa != 0 || ya != 0) move(xa, ya);
+	}
+	
+	public void render() {
+	}
+}
+
